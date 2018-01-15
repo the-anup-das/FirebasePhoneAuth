855d45460cd4b1b5f7c79911c015e6570ed06118
Anup134648 <anup.das17@yahoo.com>
2018-01-15 22:03:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hp/firebasephoneauth/SuccessActivity.java
@@ -0,0 +1,18 @@
+package com.example.hp.firebasephoneauth;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by AnupTechTips on 13/7/18.
+ */
+
+public class SuccessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success);
+    }
+}
